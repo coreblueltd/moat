a2d1c19d336f2dfe40e6b84c89c52bdd616f0fb9
Scott Helps <scott@coreblue.co.uk>
2018-09-08 19:23:13 +0100

    Add login functionality to bypass Moat

--- src/Controllers/MoatController.php
+++ src/Controllers/MoatController.php
@@ -9,6 +9,22 @@ class MoatController extends Controller
 {
     public function show()
     {
+        if(session('moat')) {
+            return redirect('/');
+        }
+
         return view('Moat::show');
+    }
+
+    public function login()
+    {
+        if(request('password') === env('MOAT_PASSWORD')) {
+            session(['moat' => request('password')]);
+            return redirect('/');
+        }
+
+        return redirect()->route('moat.show')->with([
+            'error' => 'Access Denied: incorrect password'
+        ]);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,7 @@
 
 Route::middleware('web')->name('moat.')->group(function () {
     Route::get('build-a-moat', 'CoreBlue\Moat\Controllers\MoatController@show')->name('show');
+    Route::post('build-a-moat', 'CoreBlue\Moat\Controllers\MoatController@login');
 
     Route::get('{any}', function() {
         return redirect()->route('moat.show');
