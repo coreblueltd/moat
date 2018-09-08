254e96ec91b31029b17655c5b9a52a145edeb73e
Scott Helps <scott@coreblue.co.uk>
2018-09-08 13:33:45 +0100

    Initial commit

---
+++ src/MoatServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CoreBlue\Moat;
+
+use Illuminate\Support\ServiceProvider;
+
+class MoatServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
