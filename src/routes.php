cd4945cc6d9f71b40ad6142ae11d3f399dde1aca
Scott Helps <scott@coreblue.co.uk>
2018-09-08 15:05:46 +0100

    Redirect 404 exceptions to Moat route

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,8 @@
 
 Route::middleware('web')->name('moat.')->group(function () {
     Route::get('build-a-moat', 'CoreBlue\Moat\Controllers\MoatController@show')->name('show');
+
+    Route::get('{any}', function() {
+        return redirect()->route('moat.show');
+    });
 });
